d2665c83a26110e065f16d8ad7550baec7ba24ab
Jerrodo <jerrodo.butler@gmail.com>
2018-04-03 19:13:47 -0700

    first commit

---
+++ GPA_form.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GPA_form
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cmbGrades = New System.Windows.Forms.ComboBox()
+        Me.txtCreditHours = New System.Windows.Forms.TextBox()
+        Me.btnRecord = New System.Windows.Forms.Button()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblGPA = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(74, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(36, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Grade"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 72)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Credit Hours"
+        '
+        'cmbGrades
+        '
+        Me.cmbGrades.FormattingEnabled = True
+        Me.cmbGrades.Items.AddRange(New Object() {"A", "B", "C", "D", "F"})
+        Me.cmbGrades.Location = New System.Drawing.Point(116, 23)
+        Me.cmbGrades.Name = "cmbGrades"
+        Me.cmbGrades.Size = New System.Drawing.Size(94, 21)
+        Me.cmbGrades.TabIndex = 2
+        Me.cmbGrades.Text = "Select Grade"
+        '
+        'txtCreditHours
+        '
+        Me.txtCreditHours.Location = New System.Drawing.Point(116, 65)
+        Me.txtCreditHours.Name = "txtCreditHours"
+        Me.txtCreditHours.Size = New System.Drawing.Size(94, 20)
+        Me.txtCreditHours.TabIndex = 3
+        '
+        'btnRecord
+        '
+        Me.btnRecord.AutoSize = True
+        Me.btnRecord.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnRecord.Location = New System.Drawing.Point(40, 114)
+        Me.btnRecord.Name = "btnRecord"
+        Me.btnRecord.Size = New System.Drawing.Size(170, 28)
+        Me.btnRecord.TabIndex = 4
+        Me.btnRecord.Text = "Record This Course"
+        Me.btnRecord.UseVisualStyleBackColor = True
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.AutoSize = True
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(40, 165)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(170, 28)
+        Me.btnCalculate.TabIndex = 5
+        Me.btnCalculate.Text = "Calculate GPA"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'lblGPA
+        '
+        Me.lblGPA.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblGPA.Location = New System.Drawing.Point(110, 222)
+        Me.lblGPA.Name = "lblGPA"
+        Me.lblGPA.Size = New System.Drawing.Size(100, 23)
+        Me.lblGPA.TabIndex = 6
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(74, 232)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(29, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "GPA"
+        '
+        'GPA_form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(249, 290)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.lblGPA)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.btnRecord)
+        Me.Controls.Add(Me.txtCreditHours)
+        Me.Controls.Add(Me.cmbGrades)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "GPA_form"
+        Me.Text = "GPA"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cmbGrades As ComboBox
+    Friend WithEvents txtCreditHours As TextBox
+    Friend WithEvents btnRecord As Button
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblGPA As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ GPA_form.vb
@@ -0,0 +1,89 @@
+﻿Public Class GPA_form
+
+    Dim floatingSum As Double
+    Dim numberOfCourses As Integer
+
+    'When the Record This Course button is clicked, run the following procedure
+    Private Sub btnRecord_Click(sender As Object, e As EventArgs) Handles btnRecord.Click
+        'Declare the follwing variables because 
+        Dim grades As String
+        Dim gradeValue As Integer
+        Dim hours As Integer
+        Dim courseGrade As Double
+
+        'Run validation to make sure a Grade value was selected and the credie hours value was entered.
+        '(Grade Indexes: Nothing Selected = -1, A = 0, B = 1, C = 2, D = 3, E = 4)
+        'If the user selects one of the Grades in the combo box, then check to see If the user entered a value for the Credit Hours. 
+        If (cmbGrades.SelectedIndex > -1) Then
+            If (txtCreditHours.Text = "1" Or txtCreditHours.Text = "2" Or txtCreditHours.Text = "3" Or txtCreditHours.Text = "4") Then
+                grades = cmbGrades.Text 'Assign the value entered in the Grades combo box to the grades variable
+                gradeValue = CalculateGrade(grades)
+
+                hours = CInt(txtCreditHours.Text)
+                courseGrade = CalculateCourseGrade(gradeValue, hours)
+                numberOfCourses += 1
+                floatingSum += courseGrade
+
+                Call ResetForm()
+            Else
+                MessageBox.Show("Please enter the correct number of Credit Hours.", "Invaid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            MessageBox.Show("Please select a Grade from the options provided.", "Invaid Input", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+
+    Private Sub ResetForm()
+        cmbGrades.SelectedIndex = -1
+        cmbGrades.Text = "Select Grade"
+        txtCreditHours.Clear()
+    End Sub
+
+    Function CalculateCourseGrade(gradeValue As Integer, hours As Integer) As Double
+        Dim courseGrade As Double
+        Dim finalGrade As Double
+        Dim maxPossibleGrade As Integer
+
+        maxPossibleGrade = 4 * hours '4 represents A
+        courseGrade = gradeValue * hours 'Covers B, C, D, and F
+        finalGrade = (courseGrade / maxPossibleGrade) * 4
+
+        Return finalGrade
+    End Function
+
+    'Function used to calculate the grade, using the grades variable value that was selected in the Grades combo box.
+    Function CalculateGrade(grades As String) As Integer
+        Dim gradeValue As Integer
+
+        Select Case (grades)
+            Case "A"
+                gradeValue = 4
+            Case "B"
+                gradeValue = 3
+            Case "C"
+                gradeValue = 2
+            Case "D"
+                gradeValue = 1
+            Case Else
+                gradeValue = 0
+        End Select
+
+        Return gradeValue
+    End Function
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim GPA As Double
+
+        If numberOfCourses > 0 Then
+            GPA = floatingSum / numberOfCourses
+            lblGPA.Text = GPA.ToString("N2")
+            floatingSum = 0
+            numberOfCourses = 0
+
+            Call ResetForm()
+        Else
+            MessageBox.Show("Please enter grade first", "Nothing to calculate", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+End Class
